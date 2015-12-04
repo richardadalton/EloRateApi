77d322d15fa66deb1f829fdf246c6268db904d0b
Richard Dalton <richard@Richards-MacBook-Pro.local>
2015-12-04 12:34:00 +0000

    Need dummy ID when creating dummy data

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -6,8 +6,8 @@
 open Fake
 
 #r "../packages/Suave/lib/net40/Suave.dll"
-open Suave.Http.Successful 
-open Suave.Http.Applicatives 
+open Suave.Http.Successful
+open Suave.Http.Applicatives
 open Suave.Http.RequestErrors
 open Suave.Http
 open Suave.Web
@@ -27,22 +27,22 @@ open EloRateApi.Rest
 
 let model = new CQAgent<State>(Model.initialState)
 
-Player.Create model { Name="Sandra"; Points=1100; Retired=true }    |> ignore
-Player.Create model { Name="Richard"; Points=1100; Retired=false }  |> ignore
-Player.Create model { Name="Tom"; Points=1000; Retired=false }      |> ignore
-Player.Create model { Name="Mary"; Points=1000; Retired=false }     |> ignore
-Player.Create model { Name="Harry"; Points=1000; Retired=false }    |> ignore
-Player.Create model { Name="Jane"; Points=1000; Retired=false }     |> ignore
+Player.Create model { Id = 0; Name="Sandra"; Points=1100; Retired=true }    |> ignore
+Player.Create model { Id = 0; Name="Richard"; Points=1100; Retired=false }  |> ignore
+Player.Create model { Id = 0; Name="Tom"; Points=1000; Retired=false }      |> ignore
+Player.Create model { Id = 0; Name="Mary"; Points=1000; Retired=false }     |> ignore
+Player.Create model { Id = 0; Name="Harry"; Points=1000; Retired=false }    |> ignore
+Player.Create model { Id = 0; Name="Jane"; Points=1000; Retired=false }     |> ignore
 
-Game.Create model { Winner="Sandra"; Loser="Richard" }              |> ignore
-Game.Create model { Winner="Tom"; Loser="Jane" }                    |> ignore
-Game.Create model { Winner="Mary"; Loser="Harry" }                  |> ignore
-Game.Create model { Winner="Richard"; Loser="Harry" }               |> ignore
+Game.Create model { Id = 0; Winner="Sandra"; Loser="Richard" }              |> ignore
+Game.Create model { Id = 0; Winner="Tom"; Loser="Jane" }                    |> ignore
+Game.Create model { Id = 0; Winner="Mary"; Loser="Harry" }                  |> ignore
+Game.Create model { Id = 0; Winner="Richard"; Loser="Harry" }               |> ignore
 
 
 
 let PlayerRoutes = choose [
-                        Get "players" (Player.GetAll model) 
+                        Get "players" (Player.GetAll model)
                         GetById "players" (Player.GetItem model)
                         Post "players" (Player.Create model)
                         Delete "players" (Player.DeleteItem model)
@@ -51,7 +51,7 @@ let PlayerRoutes = choose [
                         ]
 
 let GameRoutes = choose [
-                        Get "games" (Player.GetAll model) 
+                        Get "games" (Player.GetAll model)
                         GetById "games" (Player.GetItem model)
                         Post "games" (Player.Create model)
                         Delete "games" (Player.DeleteItem model)
